478d693851676290643b42843503d62cdaffdce6
David Haynes <davidhaynesvictoria@gmail.com>
2018-01-20 04:04:47 +0000

    corrected the output for how many hundreds

--- howmanyhundreds.js
+++ howmanyhundreds.js
@@ -1,9 +1,9 @@
 function howManyHundreds(num) {
-  return "How many times does 100 go into " + num + "? " + Math.floor(num / 100) + " times."
+  return Math.floor(num / 100)
 }
 
-console.log(howManyHundreds(123456));
-console.log(howManyHundreds(894));
-console.log(howManyHundreds(520));
-console.log(howManyHundreds(99));
-console.log(howManyHundreds(0));
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);

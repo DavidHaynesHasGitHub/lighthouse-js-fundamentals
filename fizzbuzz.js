deea8e9160662737e5371b9fc531135c64470402
David Haynes <davidhaynesvictoria@gmail.com>
2018-01-23 01:42:26 +0000

    adding loopylighthouse and lastindex

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,7 @@
 for(i = 1; i <= 100; i ++){
   if( i % 3 === 0){
     console.log('fizz')
-  } else if(i % 5 === 0){
+  } else if(i %  === 0){
     console.log("buzz")
   } else{
     console.log(i)}

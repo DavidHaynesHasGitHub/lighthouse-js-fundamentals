b3dce93f6b525ff1b2f1c52212e28faefd79eeb5
David Haynes <davidhaynesvictoria@gmail.com>
2018-01-23 01:56:50 +0000

    saved file

--- merge.js
+++ merge.js
@@ -1,5 +1,5 @@
 function merge (array1, array2){
-  var array3 = []
-  array3 = array1.concat(array2)
-  return array3.sort
+  var array3 = [];
+  array3 = array1.concat(array2);
+  return array3.sort();
 }
